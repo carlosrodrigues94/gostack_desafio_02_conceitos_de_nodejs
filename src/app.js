5e6d5186dc0a326c0d14ee42f45f5fa5104e0bee
Carlos Henrique Rodrigues Santos <51969060+carlosrodrigues94@users.noreply.github.com>
2020-04-08 13:16:49 -0300

    finished chalenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,26 +11,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+  const results = title
+    ? repositories.filter((repository) => repository.title.includes(title))
+    : repositories;
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  const id = uuid();
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
-  console.log(id, title, url, techs, likes);
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "ID not valid" });
+  }
+
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "ID not valid" });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
